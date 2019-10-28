3e856e284d239102f6a2823a7e82e3641c00bd1c
Michika Iranga <alexvista1234@gmail.com>
2019-10-29 00:39:22 +0530

    mid commit

--- Release.sh
+++ Release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-NEXT_VERSION=$1
-PROJECT_FOLDER=$2
-BINARY_NAME=$3
+export NEXT_VERSION=$1
+export PROJECT_FOLDER=$2
+export BINARY_NAME=$3
 cd "${PROJECT_FOLDER}"
 dotnet build && dotnet pack -p:PackageVersion=${NEXT_VERSION}
 cd bin/Debug && zip -r release.zip .
